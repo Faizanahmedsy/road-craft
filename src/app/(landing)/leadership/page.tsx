7f99fa965ab6ea0d387e4e287a90909ecf38c639
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-03-02 10:02:05 +0530

    feat: added new pages

--- src/app/(landing)/leadership/page.tsx
+++ src/app/(landing)/leadership/page.tsx
@@ -1,5 +1,6 @@
+import UnderConstruction from "@/components/shared/under-construction";
 import React from "react";
 
 export default function LeaderShipPage() {
-  return <div className="mt-20">LeaderShipPage</div>;
+  return <UnderConstruction />;
 }
